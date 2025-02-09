78729a5c3099c8c056b54fe410d277f6eca2dcbe
Denise Worthen <denise.worthen@noaa.gov>
2025-02-09 11:03:28 -0500

    test 2-step mesh creation

--- model/src/wav_shr_mod.F90
+++ model/src/wav_shr_mod.F90
@@ -75,7 +75,9 @@ module wav_shr_mod
   logical            , public :: merge_import  = .false.  !< @public logical to specify whether import fields will
                                                           !! be merged with a field provided from a file
   logical            , public :: multigrid = .false.      !< @public logical to control whether wave model is run
-                                                          !! as multigrid
+                                                         !! as multigrid
+  ! debug
+  logical, public :: twostep = .false.
 
   interface ymd2date
     module procedure ymd2date_int
