a687b66b4f8132c4d3e80a7159a0228f9c49f3b7
Denise Worthen <denise.worthen@noaa.gov>
2025-05-05 06:08:41 -0400

    add back variable w/ proper tag

--- model/src/wav_history_mod.F90
+++ model/src/wav_history_mod.F90
@@ -867,7 +867,7 @@ contains
          ]
 
     !  6   Wave-ocean layer
-    gridoutdefs(6,1:23) = [ &
+    gridoutdefs(6,1:25) = [ &
          varatts( "SXY  ", "SXX       ", "Radiation stresses xx                           ", "N m-1     ", "  ", .false.) , &
          varatts( "SXY  ", "SYY       ", "Radiation stresses yy                           ", "N m-1     ", "  ", .false.) , &
          varatts( "SXY  ", "SXY       ", "Radiation stresses xy                           ", "N m-1     ", "  ", .false.) , &
@@ -889,6 +889,8 @@ contains
          varatts( "FIC  ", "PHICE     ", "Wave to sea ice energy flux                     ", "W m-2     ", "  ", .false.) , &
          varatts( "USP  ", "USSPX     ", "Partitioned surface Stokes drift x              ", "m s-1     ", "p ", .false.) , &
          varatts( "USP  ", "USSPY     ", "Partitioned surface Stokes drift y              ", "m s-1     ", "p ", .false.) , &
+         varatts( "TOC  ", "TAUOCX    ", "Total wave to ocean stress x                    ", "Pa        ", "  ", .false.) , &
+         varatts( "TOC  ", "TAUOCY    ", "Total wave to ocean stress y                    ", "Pa        ", "  ", .false.) , &
          varatts( "USSH ", "USSHX     ", "Surface layer averaged Stokes drift x           ", "m s-1     ", "  ", .false.) , &
          varatts( "USSH ", "USSHY     ", "Surface layer averaged Stokes drift y           ", "m s-1     ", "  ", .false.)   &
          ]
