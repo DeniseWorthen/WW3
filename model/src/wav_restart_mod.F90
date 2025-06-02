f6d1eb8c694137b7cefb6a1d814da1f0f2428a03
Denise Worthen <denise.worthen@noaa.gov>
2025-06-02 15:04:36 -0400

    fix array bounds

--- model/src/wav_restart_mod.F90
+++ model/src/wav_restart_mod.F90
@@ -210,7 +210,7 @@ contains
         ierr = pio_inq_varid(pioid,  trim(vname), varid)
         call handle_err(ierr, 'inquire variable '//trim(vname))
         call pio_setframe(pioid, varid, int(1,kind=PIO_OFFSET_KIND))
-        call pio_write_darray(pioid, varid, iodesc2d, lva, ierr)
+        call pio_write_darray(pioid, varid, iodesc2d, lva(:,kk), ierr)
         call handle_err(ierr, 'put variable '//trim(vname))
       end do
     else
